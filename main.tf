308f8ffbf19aee003e7693d5806633b7b1c1cfd5
paulyan01 <126923309+paulyan01@users.noreply.github.com>
2023-10-04 20:39:41 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,20 @@
+locals{
+  linux_app=[for f in fileset("${path.module}/configs", "[^_]*.yaml") : yamldecode(file("${path.module}/configs/${f}"))]
+  linux_app_list = flatten([
+    for app in local.linux_app : [
+      for linuxapps in try(app.linux_app, []) :{
+        name=linuxapps.name
+        resource_group_name=linuxapps.resource_group
+        location=linuxapps.location
+        os_type=linuxapps.os_type
+        sku_name=linuxapps.sku_name     
+      }
+    ]
+])
+}
 
+resource "azurerm_resource_group" "azureresourcegroup" {
+for_each={for rg in local.linux_app_list:"${rg.name} => rg}  
+name     = each.value.name
+  location = each.value.location
+}
